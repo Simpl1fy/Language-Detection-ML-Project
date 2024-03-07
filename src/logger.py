c079928a997d3a8026543f4e3e90277f1590899e
Gourab <gourabgaming111@gmail.com>
2024-03-08 01:02:08 +0530

    modified the logger file

--- src/logger.py
+++ src/logger.py
@@ -3,15 +3,19 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path, exist_ok=True)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-
-LOG_FILE_PATH=os.path.join(log_path, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-
-    file_name=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
+
+# Checking if logger works or not
+if __name__=="__main__":
+    logging.info("Logging has started")
